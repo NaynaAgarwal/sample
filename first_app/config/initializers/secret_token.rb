f5ae8dfb57f26a5b4c03c514a2ff94bdf0647f29
NaynaAgarwal <nayna1914@gmail.com>
2013-07-10 12:03:38 +0530

    adding first_app to master branch

---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '90ced6d0c9383a5ace2a9eea8933fd068736689e7fc49dbf5f3fc876bb7b889359b38cd917b4f84a2ec6d087f6e54f8dd4ade11af448fd7056bc4615429e62a9'
